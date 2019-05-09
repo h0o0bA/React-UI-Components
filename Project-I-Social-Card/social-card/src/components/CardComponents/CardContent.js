a81083f4eb2a9b3108862519202c6d11c306231f
Ihab Ali <ihab@live.com>
2019-05-08 21:34:26 -0500

    project complete

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -5,8 +5,9 @@ const CardContent = () => {
   return (
     <div className="banner-content">
       <h3>Get started with React</h3>
-      <p>React makes it painless to create interactive UIs.<br /> Design simple views for each state in your applicationCache.</p>
-      <a href="https://www.reactjs.org">reactjs.org</a>
+      <p>React makes it painless to create interactive UIs.<br /> Design simple views for each state in your applicationCache.
+      <br></br><a href="https://www.reactjs.org">reactjs.org</a></p>
+      
     </div>
   );
 };
