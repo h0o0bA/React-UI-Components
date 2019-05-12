4589f02ae46b6b8dfb875a457bdd3e9ffdd6c65a
Ihab Ali <ihab@live.com>
2019-05-12 17:46:06 -0500

    Calculator project initial commit

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Display.css';
+
+const DisplayScreen = () => {
+  return (
+    <div className="display">Initial</div>
+  )
+}
+
+export default DisplayScreen;
